2722c536f1f724e13d7407f8d775e71aeb15a500
= <belqisivana7@gmail.com>
2025-09-19 14:04:09 +0700

    in and out nilai

--- bin/project2.dart
+++ bin/project2.dart
@@ -5,17 +5,22 @@ import 'package:project2/project2.dart' as project2;
 void main(List<String> arguments) {
   //print('Hello world: ${project2.calculate()}!');
 
-  var fl = List<int>.filled(4, 0);
-  fl[0] = 1;
-  fl[1] = 2;
-  fl[2] = 3;
-  fl[3] = 4;
-  //cetak
-  var gl= [1,2,3];
-  gl.add(4);
-
-  gl.remove(2);
+  // var fl = List<int>.filled(4, 0);
+  // fl[0] = 1;
+  // fl[1] = 2;
+  // fl[2] = 3;
+  // fl[3] = 4;
+  // //cetak
+  // var gl= [1,2,3];
+  // gl.add(4);
+  // gl.remove(2);
   
-  stdout.writeln(gl);
+  var setNilai1 = <int>{1,2,3,4};
+  var setNilai2 = {3,3,4,5};
+
+  print(setNilai2);
+  print(setNilai1.union(setNilai2));
+  print(setNilai1.intersection(setNilai2));
+  // stdout.writeln(gl);
   //List<int> angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
---
+++ bin/project2input.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+// void main() {
+//   var setNilai1 = <int>[];
+//   var setNilai2 = <int>[1, 2, 3];
+
+//   stdout.writeln('Jumlah data setNilai1: ');
+//   String? input1 = stdin.readLineSync();
+//   int jumlah1 = int.tryParse(input1 ?? "") ?? 0;
+
+//   for (var i = 0; i < jumlah1; i++) {
+//     stdout.writeln('Masukkan nilai ke ${i + 1} = ');
+//     String? inputNilai = stdin.readLineSync();
+//     int nilai = int.tryParse(inputNilai ?? "") ?? 0;
+//     setNilai1.add(nilai);
+//   }
+
+//   print(setNilai1);
+// }
+
+void main() {
+  var setNilai1 = <String>{};
+  stdout.writeln('Jumlah data setNilai1: ');
+  String? input1 = stdin.readLineSync();
+  int jumlah1 = int.tryParse(input1 ?? "") ?? 0;
+
+  for (var i = 0; i < jumlah1; i++) {
+    stdout.writeln('Masukkan nilai ke ${i + 1} = ');
+    String? inputNilai = stdin.readLineSync();
+    String nilai = inputNilai ?? "";
+    setNilai1.add(nilai);
+  }
+  var NilaiList = setNilai1.toList();
+  print(NilaiList);
+  
+}
