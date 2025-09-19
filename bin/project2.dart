611fe81032a7da90dfd2dd609f427d22282e9d4d
= <belqisivana7@gmail.com>
2025-09-19 11:34:10 +0700

    first commit

--- bin/project2.dart
+++ bin/project2.dart
@@ -1,5 +1,18 @@
+import 'dart:io';
+
 import 'package:project2/project2.dart' as project2;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project2.calculate()}!');
+  //print('Hello world: ${project2.calculate()}!');
+
+  var fl = List<int>.filled(4, 0);
+  fl[0] = 1;
+  fl[1] = 2;
+  fl[2] = 3;
+  fl[3] = 4;
+  //cetak
+
+  
+  stdout.writeln(fl);
+  //List<int> angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
